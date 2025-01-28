83a490effe39a70375e06c7bb53e070fc606a89d
toriesamu0 <trovao45play@gmail.com>
2025-01-27 23:22:39 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,11 @@
+// script.js
+
+function navigateTo(game) {
+    alert(`Você escolheu jogar: ${game}`);
+    // Aqui, você pode adicionar a lógica de navegação real,
+    // por exemplo, redirecionar para outra página ou abrir uma modal com o jogo.
+    // Para este exemplo, vamos apenas simular com um alerta.
+
+    // Para navegação para páginas de jogo (se houver páginas separadas):
+    // window.location.href = `${game.toLowerCase()}.html`;
+}
